d82b22a1a53bf9ac68463b85ddb890a85a54a2fe
murtazajoo <98375522+murtazajoo@users.noreply.github.com>
2023-02-22 21:50:43 +0530

    removed : . / from id url

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -39,7 +39,10 @@ function renderPosts(data) {
           </discription>
           </span>
           <br>
-          <a href="./post/?id=${post.id.replace(/[./:]/g, '');}" class="btn btn-link">read more</a>
+          <a href="./post/?id=${post.id.replace(
+            /[./:]/g,
+            ""
+          )}" class="btn btn-link">read more</a>
         </div>
       </article>`;
   });

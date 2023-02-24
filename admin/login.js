102cf4d9581f99bb13c0ad4e45e67a1c6a675f4e
murtazajoo <98375522+murtazajoo@users.noreply.github.com>
2023-02-24 21:30:41 +0530

    bug-fixed

--- admin/login.js
+++ admin/login.js
@@ -35,24 +35,8 @@ async function handleSubmit(event) {
     return;
   }
   setCookie("key", data["access_token"], 10);
-  window.location.href = "./options";
+  window.location.href = "./options/";
 }
 
 const form = document.querySelector("form");
 form.addEventListener("submit", handleSubmit);
-
-// function getCookie(cname) {
-//   let name = cname + "=";
-//   let decodedCookie = decodeURIComponent(document.cookie);
-//   let ca = decodedCookie.split(";");
-//   for (let i = 0; i < ca.length; i++) {
-//     let c = ca[i];
-//     while (c.charAt(0) == " ") {
-//       c = c.substring(1);
-//     }
-//     if (c.indexOf(name) == 0) {
-//       return c.substring(name.length, c.length);
-//     }
-//   }
-//   return "";
-// }
